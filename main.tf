614662413b3eabecc827eb7857e9d9fff99b9501
potzenhotz <lukasmuessle@gmail.com>
2020-08-21 17:39:22 +0200

    added main

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+
+locals {
+  ami_id        = "ami-04cf43aca3e6f3de3"
+  region        = "eu-central-1"
+  subnet_region = "eu-central-1a"
+}
+
 resource "aws_instance" "example_machine" {
   ami                    = local.ami_id
   instance_type          = "t2.micro"
