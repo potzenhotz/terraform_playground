fde3ab5f5d28395c7956d1e7ce03c0d2960f1c2d
potzenhotz <lukasmuessle@gmail.com>
2020-08-21 17:39:28 +0200

    removed line

--- subnets.tf
+++ subnets.tf
@@ -8,7 +8,6 @@ resource "aws_subnet" "example_machine" {
 }
 resource "aws_route_table" "example_machine" {
   vpc_id = aws_vpc.example_machine.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.example_machine.id
